f559400f825c1a27a28b2817ee68a41a9960d253
student <student@mail.ru>
2020-02-22 03:04:55 +0300

    work_07.02.2020

---
+++ self_task/work_07.02.2020/projects/discret01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Discret01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 5, 27, 38, 24, 35 };
+            Sort.Bubble(array);
+            Console.WriteLine("Bubble Sort");
+            foreach (var i in array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            int[] array1 = new int[] { 5, 27, 38, 24, 35 };
+            Sort.Selection(array1);
+            Console.WriteLine("Selection Sort");
+            foreach (var i in array1)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ self_task/work_07.02.2020/projects/discret01/Sort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Discret01
+{
+    class Sort
+    {
+        public static void Bubble(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+            bool swapDetected = false;
+            while (true)
+            {
+                swapDetected = false;
+                for (int index = 0; index < array.Length - 1; index++)
+                {
+                    if (array[index] < array[index + 1])
+                    {
+                        int tmp = array[index];
+                        array[index] = array[index + 1];
+                        array[index + 1] = tmp;
+
+                        swapDetected = true;
+                    }
+                }
+                if (!swapDetected)
+                    break;
+            }
+        }
+
+        public static void Selection(int[] array)
+        {
+            if (array == null || array.Length < 2)
+                return;
+            for (int index =0; index < array.Length; index++)
+            {
+                int minIndex = index;
+                for (int next = index + 1; next < array.Length; next++)
+                    if (array[minIndex] < array[next])
+                        minIndex = next;
+                if(minIndex != index)
+                {
+                    int tmp = array[index];
+                    array[index] = array[minIndex];
+                    array[minIndex] = tmp;
+                }
+            }
+        }
+    }
+}
